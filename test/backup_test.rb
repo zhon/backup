6dd50088290e2193ed1e7f0d9ec776efb4c0bf67
Zhon <zhon@xmission.com>
2019-07-30 19:53:45 -0600

    Added implementation

--- test/backup_test.rb
+++ test/backup_test.rb
@@ -1,11 +1,15 @@
 require "test_helper"
 
-class BackupTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Backup::VERSION
+
+module Backup
+
+  describe 'backup' do
+
+    it "does something" do
+      assert false
+    end
+
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
 end
+
